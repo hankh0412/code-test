dd3dae4b26b474c9af0806ecb4cc8b36a3d6e8aa
hankh0412 <hankh0412@naver.com>
2021-07-26 13:27:09 +0900

    Format document

--- JavaScript/Promise.js
+++ JavaScript/Promise.js
@@ -1,26 +1,27 @@
 var jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const { window } = new JSDOM();
-const { document } = (new JSDOM('')).window;
+const { document } = new JSDOM("").window;
 global.document = document;
 
-var $ = jQuery = require('jquery')(window);
+var $ = (jQuery = require("jquery")(window));
 
 function getData() {
-    return new Promise(function(resolve, reject) {
-      $.get('https://jsonplaceholder.typicode.com/users/1', function(response) {
-        if (response) {
-          resolve(response);    // 여기가 호출되면 아래 reject는 호출되지 않는다.
-        }
-        reject(new Error("Request is failed"));
-      });
+  return new Promise(function (resolve, reject) {
+    $.get("https://jsonplaceholder.typicode.com/users/1", function (response) {
+      if (response) {
+        resolve(response); // 여기가 호출되면 아래 reject는 호출되지 않는다.
+      }
+      reject(new Error("Request is failed"));
     });
-  }
-  
-  // 위 $.get() 호출 결과에 따라 'response' 또는 'Error' 출력
-  getData().then(function(data) {
+  });
+}
+
+// 위 $.get() 호출 결과에 따라 'response' 또는 'Error' 출력
+getData()
+  .then(function (data) {
     console.log(data); // response 값 출력
-  }).catch(function(err) {
+  })
+  .catch(function (err) {
     console.error(err); // Error 출력
   });
-  
